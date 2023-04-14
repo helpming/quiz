f07814838702b8523af6a7f29c99d7ec0601599d
Yang HeeKwon <126805059+helpming@users.noreply.github.com>
2023-04-14 11:11:01 +0900

    [level 0] Title: 369게임, Time: 0.08 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.py
+++ 프로그래머스/lv0/120891. 369게임/369게임.py
@@ -1,6 +1,11 @@
+import re
+
 def solution(order):
     # count = str(order).count('3')
     # count += str(order).count('6')
     # count += str(order).count('9')
     # return count
-    return sum(list(map(lambda x: str(order).count(x), '369')))
+    
+    # return sum(list(map(lambda x: str(order).count(x), '369')))
+    
+    return len(re.sub('[^369]', '', str(order)))
