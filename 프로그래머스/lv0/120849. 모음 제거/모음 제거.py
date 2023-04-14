f81288be3986d6a90ce0f7435a23a3b605d88ae7
Yang HeeKwon <126805059+helpming@users.noreply.github.com>
2023-04-14 09:26:08 +0900

    [level 0] Title: 모음 제거, Time: 0.08 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.py
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.py
@@ -1,2 +1,5 @@
+import re
+
 def solution(my_string):
-    return my_string.replace('a', '').replace('e', '').replace('i', '').replace('o', '').replace('u', '')
+    # return my_string.replace('a', '').replace('e', '').replace('i', '').replace('o', '').replace('u', '')
+    return re.sub(r'[aeiou]', '', my_string)
