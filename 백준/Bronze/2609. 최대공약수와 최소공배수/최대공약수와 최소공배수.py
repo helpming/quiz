936c2188a223e86be27079790ae2df16d0fd9903
Yang HeeKwon <126805059+helpming@users.noreply.github.com>
2023-06-08 13:00:06 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,6 +1,11 @@
-from math import gcd, lcm
-
 p, q = map(int, input().split(' '))
 
-print(gcd(p,q))
-print(lcm(p,q))
+for d in range(min(p, q), 0, -1):
+  if p%d == 0 and q%d == 0:
+    print(d)
+    break
+
+for m in range(max(p, q), p*q+1, max(p, q)):
+  if m%p == 0 and m%q == 0:
+    print(m)
+    break 
